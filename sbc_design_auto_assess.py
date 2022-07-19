910213caf088636dd982ddead9e7c066a8caf421
liliangnike <60680241+liliangnike@users.noreply.github.com>
2022-07-19 11:21:50 +0800

    Update sbc_design_auto_assess.py

--- sbc_design_auto_assess.py
+++ sbc_design_auto_assess.py
@@ -21,44 +21,47 @@ from time import gmtime, strftime
 
 this=os.path.basename(sys.argv[0])
 
+
+# global variables
+sigaling_artifact = ''
+media_artifact = ''
+sbc_release = ''
+
+
 #################################################################################
 def parse_validate_opts():
-        # Module to parse the option value and then validate it #
         usage= '%prog options.'
 
-        global action, check_en, config_file
-        description= 'This script is used for growing a pair of LCP hosts, supports both bulk configuration and interactive configuration.'
+        global sigaling_artifact, media_artifact, sbc_release
+        description= 'The script is used for SBC L3 Design exam assignment assessment automatically.'
         parser = optparse.OptionParser(usage=usage, description=description)
 
         # Note: defaults action="store", type="string", dest="OPTION"
-        parser.add_option("-a", "--action", dest='action', default='interactive', help="action interactive|bulk|gen_template|flex_bulk|gen_flex_bulk_template")
-        parser.add_option("-c", "--check", action='store_true', dest='check_en', default=False, help="enable check option")
-        parser.add_option("-f", "--file", action='store', dest='filename', help="file of bulk configuration")
+        parser.add_option("-s", "--sig_artifact", help="SBC signaling artifacts zip file")
+        parser.add_option("-m", "--media_artifact", help="SBC media artifacts zip file")
+        parser.add_option("-r", "--sbc_release", help="SBC release")
 
         # validate options
         try:
                 (options, args) = parser.parse_args()
 
-                if (options.action != 'interactive') and (options.action != 'bulk') and (options.action != 'gen_template') \
-                        and (options.action != 'flex_bulk') and (options.action != 'gen_flex_bulk_template') :
-                        log('Action: ' + options.action + ' is not valid.')
-                        parser.print_help()
-                        sys.exit(1)
-
         except Exception, exc:
                 err_log('Invalid options passed')
                 log(traceback.format_exc())
                 sys.exit(1)
-        action = options.action
-        check_en = options.check_en
-        config_file = options.filename
-        
+                
+        sigaling_artifact = options.sig_artifact
+        media_artifact = options.media_artifact
+        sbc_release = options.sbc_release
 
+#################################################################################
+def signaling_actifact_process():
+        
 
 #################################################################################
 def main():
         parse_validate_opts()
-        signaling_actifacts_process()
+        signaling_actifact_process()
         media_artifacts_process()
         network_validation()
         domain_validation()
